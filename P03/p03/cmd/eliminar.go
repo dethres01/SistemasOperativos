96f48eede3eeab574235961625ae08d284d64bc4
Hirane <diego.preciado@alumno.udg.mx>
2022-02-24 10:32:21 -0600

    eliminar codigo postal

--- P03/p03/cmd/eliminar.go
+++ P03/p03/cmd/eliminar.go
@@ -5,29 +5,70 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 
 	"github.com/spf13/cobra"
 )
 
 // eliminarCmd represents the eliminar command
 var eliminarCmd = &cobra.Command{
-	Use:   "eliminar",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "eliminar [NombreFichero][CodigoPostal]",
+	Short: "Este comando elimina del fichero todos los registros que coincidan con el [CodigoPostal] ingresado",
+	Long:  `Este comando elimina del fichero todos los registros que coincidan con el [CodigoPostal] ingresado.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("eliminar called")
+		filename, err := cmd.Flags().GetString("nombre_fichero")
+		if err != nil {
+			log.Fatal(err)
+		}
+		postalCode, err := cmd.Flags().GetString("codigo_postal")
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Eliminando del fichero " + filename + " todos los registros con el codigo postal " + postalCode)
+		eliminarRegistros(filename, postalCode)
+
 	},
 }
 
+func eliminarRegistros(filename string, postalcode string) {
+	if _, err := os.Stat("json_files/" + filename + ".json"); os.IsNotExist(err) {
+		log.Fatal("No existe el fichero")
+	}
+	// we want to read the file
+	data, err := ioutil.ReadFile("json_files/" + filename + ".json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fields := make(map[string]interface{})
+
+	err = json.Unmarshal(data, &fields)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// test
+	// print  a field
+	fmt.Println(fields[postalcode])
+	// mantain format of the file
+	// delete the field
+	delete(fields, postalcode)
+	json_data, err := json.MarshalIndent(fields, "", "    ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = ioutil.WriteFile("json_files/"+filename+".json", json_data, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
 func init() {
 	rootCmd.AddCommand(eliminarCmd)
-
+	eliminarCmd.Flags().StringP("nombre_fichero", "n", "", "Nombre del fichero") //name of the file
+	eliminarCmd.Flags().StringP("codigo_postal", "c", "", "Codigo Postal")       //postalcode
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
